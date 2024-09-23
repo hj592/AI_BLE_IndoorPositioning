d92257ff426afba7b18aad99954b9446119316ab
hj592 <ghn07096@naver.com>
2024-09-23 17:00:01 +0900

    add predictor

---
+++ src/main/java/com/example/bleLocationSystem/model/Predictor.java
@@ -0,0 +1,27 @@
+package com.example.bleLocationSystem.model;
+
+public class Predictor {
+    public native float predictDistance(float[] rssiValues);
+
+    static {
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\c10.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\asmjit.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\libiomp5md.dll");
+
+        //System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\torch_global_deps.dll");
+        //System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\uv.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\fbgemm.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\libiompstubs5md.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\uv.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\torch_cpu.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\torch.dll");
+        System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\c10.dll");
+
+        //System.load("C:\\Users\\heejin\\Documents\\libtorch-win-shared-with-deps-2.4.1+cpu\\libtorch\\lib\\pytorch_jni.dll");
+
+        //System.load("C:\\Users\\heejin\\Desktop\\chart\\LibtorchPredictor\\out\\production\\LibtorchPredictor\\torch_cpu.dll");  // DLL 이름
+
+        //System.loadLibrary("lstmcnnTest");  // DLL 이름
+        System.load("C:\\Users\\heejin\\Desktop\\chart\\LibtorchPredictor\\lstmcnnTest.dll");  // DLL 이름
+    }
+}
